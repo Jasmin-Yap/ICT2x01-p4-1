5271d3861093a7746a74c32881b318bdb22970a4
Jasticar <jasmin.yap06@gmail.com>
2021-11-15 11:30:02 +0800

    Created Web Portal Base

---
+++ Web-Portal/server.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from controllers import *
+
+app = Flask(__name__, template_folder="views")
+
+@app.route('/')
+def connection():
+    return render_template('dashboard.html')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
+ 
