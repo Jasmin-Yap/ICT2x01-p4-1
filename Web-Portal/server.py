876b1ea1198e4673f8b76a8b92aa00357ca0a301
AfnanMuhd <afnan.muhd98@gmail.com>
2021-11-23 13:04:34 +0800

    added new files for blockly

--- Web-Portal/server.py
+++ Web-Portal/server.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def connection():
     return render_template('dashboard.html')
+@app.route('/blockly')
+def blockly():
+    return render_template('blockly.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
