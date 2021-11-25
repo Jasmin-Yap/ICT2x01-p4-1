27b06820282d15c19f639d750167f64ef5aca9ff
Jasticar <jasmin.yap06@gmail.com>
2021-11-25 21:43:02 +0800

    cleaned up feature branch
    removed unneeded code from server.py and updated base.html

--- Web-Portal/server.py
+++ Web-Portal/server.py
@@ -14,17 +14,15 @@ token = token_controller
 def connection():
     return render_template('conn.html')
 
+# route to be moved to connection controller
 @app.route('/dashboard')
 def dash():
     token.generate_token()
-    logging.debug("verify token (True): {0}".format(token.verify_token(token.auth_token.get_token())))
-    logging.debug("verify token (False): {0}".format(token.verify_token("N2u-9nAzeR6brtz-MNts7g")))
     return render_template('dashboard.html')
 
 @app.route('/connection')
 def end_session():
     token.clear_token()
-    logging.debug("token after deletion: {0}".format(token.auth_token.get_token()))
     return render_template('conn.html')
 
 if __name__ == '__main__':
