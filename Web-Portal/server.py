ffb34887856367c98d8689336ae98669c06ab1ea
youfu123 <YFyf0927!2#>
2021-12-05 02:11:13 +0800

    feat!: Created feature/statsdisplay branch

--- Web-Portal/server.py
+++ Web-Portal/server.py
@@ -9,15 +9,18 @@ app.register_blueprint(scoreboard_controller.scoreboard_page)
 app.register_blueprint(dashboard_controller.dashboard_page)
 app.register_blueprint(maze_controller.mazecreator_page)
 
+
 @app.route('/')
 def connection():
     return render_template('connection.html')
+
 
 @app.route('/connection')
 def end_session():
     token_controller.clear_token()
     maze_controller.clear_custom_mazes()
     return render_template('connection.html')
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
