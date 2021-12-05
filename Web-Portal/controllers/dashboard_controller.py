9ba8b99dfff4b193166480736364b1ef14d1c2d5
youfu123 <YFyf0927!2#>
2021-12-06 02:27:44 +0800

    updates: gone...

--- Web-Portal/controllers/dashboard_controller.py
+++ Web-Portal/controllers/dashboard_controller.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template, Flask, request, jsonify
-from controllers import token_controller
-
+from controllers import token_controller, connection_controller
+from models import blockly
 
 dashboard_page = Blueprint('dashboard_page', __name__)
-stuff_page = Blueprint('stuff_page', __name__)
+block_arr = []
 
 
 def get_stats(i):
@@ -17,12 +17,26 @@ def get_stats(i):
     return stats_to_html
 
 
-@dashboard_page.route('/dashboard', methods=['POST', 'GET'])
+def get_instructions():
+    instruction = block_arr
+    return instruction
+
+
+@dashboard_page.route('/dashboard', methods=['GET', 'POST'])
+def blocklyPage():
+    if request.method == 'POST':
+        data = request.get_json()
+        instructions = blockly.Blockly(data['instructions'])
+        #obj.set_instructions(get_instructions(data['instructions']))
+        #connection_controller.send_instruction(obj)
+        result = {'processed': 'true'}
+        return jsonify(result)
+    token_controller.generate_token()
+    return render_template('dashboard.html', data=get_instructions())
+
+@dashboard_page.route('/stats', methods=['POST'])
 def display_dashboard():
     if request.method == "POST":
         data = request.get_json()
         results = get_stats()
         return jsonify(results)
-
-    token_controller.generate_token()
-    return render_template('dashboard.html', render_stats=get_stats())
