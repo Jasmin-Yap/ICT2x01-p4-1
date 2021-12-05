8530532577f8efef2c024aad18f5bbb43b05ff61
youfu123 <YFyf0927!2#>
2021-12-05 18:06:41 +0800

    updates: added scheduler example

--- Web-Portal/controllers/dashboard_controller.py
+++ Web-Portal/controllers/dashboard_controller.py
@@ -1,11 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, Flask
 from controllers import token_controller
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
 dashboard_page = Blueprint('dashboard_page', __name__)
 
 
+def receive_stats():
+    print('I am working...')
+
+
 @dashboard_page.route('/dashboard')
 def display_dashboard():
+    scheduler = BackgroundScheduler()
+    job = scheduler.add_job(receive_stats, 'interval', seconds=3)
+    scheduler.start()
     token_controller.generate_token()
     return render_template('dashboard.html')
