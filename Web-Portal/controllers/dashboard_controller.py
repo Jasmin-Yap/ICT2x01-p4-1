71ca1ddbd35c2c564d5b75e4020b57e5cc59aa80
Jasticar <jasmin.yap06@gmail.com>
2021-12-06 03:41:50 +0800

    created maze display
    - created maze display on dashboard
    - created button to switch display between different maps (including custom)
    - create tojson function for maze.py
    - changed layout of dashboard to fit maze display
    - created a route to get the maze data
    - cleaned up media folder

--- Web-Portal/controllers/dashboard_controller.py
+++ Web-Portal/controllers/dashboard_controller.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, jsonify, request
-from controllers import token_controller, connection_controller
+from controllers import token_controller, connection_controller, maze_controller
 from models import blockly
 
 dashboard_page = Blueprint('dashboard_page', __name__)
@@ -18,5 +18,9 @@ def blocklyPage():
         #connection_controller.send_instruction(obj)
         result = {'processed': 'true'}
         return jsonify(result)
+
     return render_template('dashboard.html', data=get_instructions())
 
+@dashboard_page.route('/MazeData')
+def getMazeDate():
+    return jsonify(maze_controller.get_mazes())
